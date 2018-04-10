2c405e6fac3700a4bf6387eba59d34f3e367ce9d
Kathy Hudson <khitx@icloud.com>
2018-04-10 19:00:31 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -10,7 +10,7 @@ class MusicLibraryController
     input = ""
 
     while input != "exit"
-      puts "Welcome to Your music Library!"
+      puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
       puts "To list all of the genres in your library, enter 'list genres'."
