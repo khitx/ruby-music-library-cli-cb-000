8752e2565b299e4dd4c339f1ecac8a373135ce41
Kathy Hudson <khitx@icloud.com>
2018-04-10 18:22:51 +0000

    automatically backed up by learn

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -1,9 +1,8 @@
 class Artist
-  #extend Concerns::Findable::ClassMethods
-  #include Concerns::Findable::InstanceMethods
   extend Concerns::Findable
 
-  attr_accessor :name, :songs
+  attr_accessor :name
+  attr_reader :songs
   @@all = []
 
   def initialize(name)
